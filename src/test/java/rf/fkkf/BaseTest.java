c6ec1de49f79078715440fac06047a0121389499
FkkfRf <FkkfRf@gmail.com>
2022-10-13 23:08:06 +1100

    1st part. Parameterized Test

--- src/test/java/rf/fkkf/BaseTest.java
+++ src/test/java/rf/fkkf/BaseTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 public class BaseTest {
     @BeforeAll
     static void setUp() {
-        Configuration.baseUrl = "https://world-weather.ru/";
+        Configuration.baseUrl = "https://world-weather.ru";
         Configuration.browserSize = "1920x1986";
         Configuration.holdBrowserOpen = true;
     }
